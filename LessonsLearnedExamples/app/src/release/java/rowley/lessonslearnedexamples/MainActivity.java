d7250a8e70521ed1e765634860a2ddc22630b20d
Joe Rowley <joe.rowley@bodybuilding.com>
2015-02-03 22:00:12 -0700

    Finishing out basic gradle leveraging example

--- LessonsLearnedExamples/app/src/release/java/rowley/lessonslearnedexamples/MainActivity.java
+++ LessonsLearnedExamples/app/src/release/java/rowley/lessonslearnedexamples/MainActivity.java
@@ -5,6 +5,6 @@ import android.widget.Toast;
 public class MainActivity extends MainActivityParent {
     @Override
     protected void showBuildType() {
-        Toast.makeText(this, R.string.release_notification, Toast.LENGTH_SHORT);
+        Toast.makeText(this, R.string.release_notification, Toast.LENGTH_SHORT).show();
     }
 }
